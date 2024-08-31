5fc2328fb6154bb392567e9868623a74c784de39
sujnkim <blackout3210@naver.com>
2024-08-31 14:29:18 +0900

    init: project setting

---
+++ src/main/java/swyp/swyp6_team7/Swyp6Team7Application.java
@@ -0,0 +1,13 @@
+package swyp.swyp6_team7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swyp6Team7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swyp6Team7Application.class, args);
+	}
+
+}
---
+++ src/test/java/swyp/swyp6_team7/Swyp6Team7ApplicationTests.java
@@ -0,0 +1,13 @@
+package swyp.swyp6_team7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swyp6Team7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
