1f1ff16c462fb3803651ac8b87918179d5c0010a
Yujeong <22mallow3@gmail.com>
2024-09-10 10:23:28 +0900

    feat: LoginController 추가 - 로그인 API 구현

---
+++ src/main/java/swyp/swyp6_team7/auth/controller/LoginController.java
@@ -0,0 +1,22 @@
+package swyp.swyp6_team7.auth.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import swyp.swyp6_team7.auth.dto.LoginRequestDto;
+import swyp.swyp6_team7.auth.service.LoginService;
+
+@RestController
+@RequestMapping("/auth")
+public class LoginController {
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto) {
+        String token = loginService.login(loginRequestDto);
+        return ResponseEntity.ok(token);  // 로그인 성공 시 JWT 토큰 반환
+    }
+}
