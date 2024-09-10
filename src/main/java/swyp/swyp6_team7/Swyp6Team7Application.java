0fff2d525b1a7eefddc7c1ba612b5922c703c402
sujnkim <blackout3210@naver.com>
2024-09-10 22:46:28 +0900

    feat: 메인 Application에 EnableJpaAuditing 어노테이션 추가
    게시글의 생성, 수정 시간을 구현하기 위해 추가함

--- src/main/java/swyp/swyp6_team7/Swyp6Team7Application.java
+++ src/main/java/swyp/swyp6_team7/Swyp6Team7Application.java
@@ -2,12 +2,14 @@ package swyp.swyp6_team7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Swyp6Team7Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Swyp6Team7Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Swyp6Team7Application.class, args);
+    }
 
 }
