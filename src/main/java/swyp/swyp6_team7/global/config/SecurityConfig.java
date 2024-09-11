1c19e94268765cda6f0f222376a3cdaa26525c8b
Yujeong <22mallow3@gmail.com>
2024-09-11 18:49:47 +0900

    feat: SecurityConfig에 JWT 필터 추가 및 토큰 기반 인증 설정

--- src/main/java/swyp/swyp6_team7/global/config/SecurityConfig.java
+++ src/main/java/swyp/swyp6_team7/global/config/SecurityConfig.java
@@ -17,11 +17,10 @@ import swyp.swyp6_team7.auth.jwt.JwtProvider;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final JwtProvider jwtProvider;
+
     private final JwtFilter jwtFilter;
 
-    public SecurityConfig(JwtProvider jwtProvider, JwtFilter jwtFilter) {
-        this.jwtProvider = jwtProvider;
+    public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
     }
     @Bean
@@ -30,7 +29,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안함
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/login", "/api/users/new").permitAll() // 로그인, 회원가입 경로는 허용
+                        .requestMatchers("/api/login", "/api/users/new","/api/refresh-token").permitAll() // 로그인, 회원가입, 토큰 재발급 경로 경로는 허용
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
