3e75ea91538b041858f566c5f26056a6e9ba1444
Yujeong <22mallow3@gmail.com>
2024-09-11 16:42:47 +0900

    feat: 중복 확인 결과에 따른 에러 처리

--- src/main/java/swyp/swyp6_team7/member/controller/MemberController.java
+++ src/main/java/swyp/swyp6_team7/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package swyp.swyp6_team7.member.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import swyp.swyp6_team7.member.dto.UserRequestDto;
 import swyp.swyp6_team7.member.entity.Users;
@@ -18,9 +19,13 @@ public class MemberController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<String> signUp(@RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<Map<String, Object>> signUp(@RequestBody UserRequestDto userRequestDto) {
         // DTO 객체를 사용하여 회원 가입 처리
-        Map<String, Object> response = memberService.signUp(userRequestDto);
-        return ResponseEntity.ok("User registered successfully");
+        try {
+            Map<String, Object> response = memberService.signUp(userRequestDto);
+            return ResponseEntity.ok(response);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("error",e.getMessage()));  // 409 Conflict 반환
+        }
     }
 }
