950814cd44f1f7d64a073e09c9d2ee2c7f605ea1
Yujeong <22mallow3@gmail.com>
2024-09-14 12:09:01 +0900

    feat: 이메일 중복 확인 API 엔드포인트 추가

--- src/main/java/swyp/swyp6_team7/member/controller/MemberController.java
+++ src/main/java/swyp/swyp6_team7/member/controller/MemberController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class MemberController {
     private final MemberService memberService;
 
@@ -18,7 +18,7 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @PostMapping("/new")
+    @PostMapping("/users/new")
     public ResponseEntity<Map<String, Object>> signUp(@RequestBody UserRequestDto userRequestDto) {
         // DTO 객체를 사용하여 회원 가입 처리
         try {
@@ -26,6 +26,16 @@ public class MemberController {
             return ResponseEntity.ok(response);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("error",e.getMessage()));  // 409 Conflict 반환
+        }
+    }
+    // 이메일 중복 확인 엔드포인트
+    @GetMapping("/users-email")
+    public ResponseEntity<String> checkEmailDuplicate(@RequestParam("email") String email) {
+        try {
+            memberService.checkEmailDuplicate(email);  // 이메일 중복 확인 서비스 호출
+            return ResponseEntity.ok("사용 가능한 이메일입니다.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());  // 이미 사용 중인 이메일
         }
     }
 }
