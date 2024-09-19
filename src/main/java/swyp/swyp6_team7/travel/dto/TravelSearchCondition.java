3f6d13122f6978ec03389db4cc073d42aefbf242
sujnkim <blackout3210@naver.com>
2024-09-19 17:40:45 +0900

    feat: TravelSearchCondition에 빌더를 클래스레벨로 수정함
    태그필터가 포함되지 않은 검색 요청시 tags의 값을 빈리스트로 설정

--- src/main/java/swyp/swyp6_team7/travel/dto/TravelSearchCondition.java
+++ src/main/java/swyp/swyp6_team7/travel/dto/TravelSearchCondition.java
@@ -1,37 +1,24 @@
 package swyp.swyp6_team7.travel.dto;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.domain.PageRequest;
 
-import java.awt.print.Pageable;
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 @Getter
 public class TravelSearchCondition {
 
     private String keyword;
     private PageRequest pageRequest;
-    private List<String> tags;
-    //태그 -> and, 완전 일치
+    @Builder.Default
+    private List<String> tags = new ArrayList<>();
     //장소 -> 국내, 해외
     //인원 -> 
     //기간 -> 일주일 이하, 1~4주, 한달 이상
     //정렬 -> 추천순, 최신순, 등록일순, 정확도순
-
-
-    @Builder
-    public TravelSearchCondition(
-            String keyword,
-            PageRequest pageRequest,
-            List<String> tags
-    ) {
-        this.keyword = keyword;
-        this.pageRequest = pageRequest;
-        this.tags = tags;
-    }
 
 }
