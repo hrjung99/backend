2b62739111b5e0370e59693ac548615de123fe43
sujnkim <blackout3210@naver.com>
2024-09-19 22:32:39 +0900

    style: securityFilterChain 줄바꿈

--- src/main/java/swyp/swyp6_team7/global/config/SecurityConfig.java
+++ src/main/java/swyp/swyp6_team7/global/config/SecurityConfig.java
@@ -5,15 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import swyp.swyp6_team7.auth.jwt.JwtFilter;
-import swyp.swyp6_team7.auth.jwt.JwtProvider;
 
 
 @Configuration
@@ -25,6 +21,7 @@ public class SecurityConfig {
     public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors().and() // CORS 허용
@@ -32,13 +29,21 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안함
                 .authorizeHttpRequests(auth -> auth
                         // 로그인, 회원가입, 토큰 재발급, kakao 로그인 요청 경로 경로는 허용
-                        .requestMatchers("/api/login", "/api/users/new","/api/refresh-token","/login/oauth/kakao/**","/error","/api/users-email").permitAll()
+                        .requestMatchers(
+                                "/api/login",
+                                "/api/users/new",
+                                "/api/refresh-token",
+                                "/login/oauth/kakao/**",
+                                "/error",
+                                "/api/users-email"
+                        ).permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
 
         return http.build();
     }
+
     // 전역 CORS 설정
     @Bean
     public WebMvcConfigurer corsConfigurer() {
