d254ba8172c6ef02308f1ede130df24466081033
sujnkim <blackout3210@naver.com>
2024-09-21 02:01:17 +0900

    feat: TravelUpdateRequest DTO 디폴트값 설정

--- src/main/java/swyp/swyp6_team7/travel/dto/request/TravelUpdateRequest.java
+++ src/main/java/swyp/swyp6_team7/travel/dto/request/TravelUpdateRequest.java
@@ -1,19 +1,24 @@
 package swyp.swyp6_team7.travel.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TravelUpdateRequest {
 
     private String title;
     private String summary;
     @NotNull
-    private List<String> tags;
+    @Builder.Default
+    private List<String> tags = new ArrayList<>();
     private String details;
     private LocalDateTime dueDateTime;
     private LocalDate travelStartAt;
