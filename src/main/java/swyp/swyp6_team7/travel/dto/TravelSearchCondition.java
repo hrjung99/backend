e21cc22f6b2ddf63632463c88e9e9d81e2b1c9cb
sujnkim <blackout3210@naver.com>
2024-09-21 23:12:26 +0900

    fix: TravelSearchCondition에서 tags가 null일 때 발생하는 에러 수정
    빌더 사용 시 tags가 null인 경우 빈 리스트를 전달하도록 controller 수정

--- src/main/java/swyp/swyp6_team7/travel/dto/TravelSearchCondition.java
+++ src/main/java/swyp/swyp6_team7/travel/dto/TravelSearchCondition.java
@@ -1,24 +1,33 @@
 package swyp.swyp6_team7.travel.dto;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 @Getter
 public class TravelSearchCondition {
 
-    private String keyword;
     private PageRequest pageRequest;
-    @Builder.Default
-    private List<String> tags = new ArrayList<>();
+    private String keyword;
+    private List<String> tags;
+
     //장소 -> 국내, 해외
-    //인원 -> 
-    //기간 -> 일주일 이하, 1~4주, 한달 이상
+    //인원 -> maxPerson
+    //성별 -> GenderType
+    //기간 -> PeriodType
     //정렬 -> 추천순, 최신순, 등록일순, 정확도순
+
+
+    @Builder
+    public TravelSearchCondition(PageRequest pageRequest, String keyword, List<String> tags) {
+        this.pageRequest = pageRequest;
+        this.keyword = keyword;
+        this.tags = tags;
+    }
 
 }
