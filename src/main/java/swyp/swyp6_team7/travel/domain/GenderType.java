b0fe4e705c0b37c5cdf6c19df82eb8ad4c67e52e
sujnkim <blackout3210@naver.com>
2024-09-21 23:14:44 +0900

    feat: Travel GenderType, PeriodType Enum 수정
    description으로 Enum 상수를 알아내는 메서드 of 추가
    비즈니스 로직에 사용되는 Enum에 해당하지 않을 경우를 위해 NONE 추가
    description을 반환하는 toString 메서드 추가

--- src/main/java/swyp/swyp6_team7/travel/domain/GenderType.java
+++ src/main/java/swyp/swyp6_team7/travel/domain/GenderType.java
@@ -3,14 +3,29 @@ package swyp.swyp6_team7.travel.domain;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+
 @Getter
 @RequiredArgsConstructor
 public enum GenderType {
 
     MIXED("혼성"),
     WOMAN_ONLY("여자만"),
-    MAN_ONLY("남자만");
+    MAN_ONLY("남자만"),
+    NONE("없음");
 
-    private final String name;
+    private final String description;
+
+    public static GenderType of(String type) {
+        return Arrays.stream(values())
+                .filter(e -> e.description.equals(type))
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
 
 }
--- src/main/java/swyp/swyp6_team7/travel/domain/PeriodType.java
+++ src/main/java/swyp/swyp6_team7/travel/domain/PeriodType.java
@@ -3,6 +3,8 @@ package swyp.swyp6_team7.travel.domain;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+
 @Getter
 @RequiredArgsConstructor
 public enum PeriodType {
@@ -10,9 +12,17 @@ public enum PeriodType {
     ONE_WEEK("일주일 이하"),
     TWO_WEEKS("1~2주"),
     THREE_WEEKS("3~4주"),
-    MORE_THAN_MONTH("한 달 이상");
+    MORE_THAN_MONTH("한 달 이상"),
+    NONE("없음");
 
     private final String description;
+
+    public static PeriodType of(String type) {
+        return Arrays.stream(values())
+                .filter(e -> e.description.equals(type))
+                .findFirst()
+                .orElse(NONE);
+    }
 
     @Override
     public String toString() {
