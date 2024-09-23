ad46e2fdd78ba0d122595ffd20e3fcb75b64cb3d
sujnkim <blackout3210@naver.com>
2024-09-23 17:37:26 +0900

    fix(travel): TravelSearchCondition 오류 수정 및 보완
    personTypes가 request로 주어지지 않을 때 빈 리스트를 설정하도록 수정
    각 필터를 만들 때 distinct 조건을 추가함

--- src/main/java/swyp/swyp6_team7/travel/dto/TravelSearchCondition.java
+++ src/main/java/swyp/swyp6_team7/travel/dto/TravelSearchCondition.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import swyp.swyp6_team7.travel.domain.GenderType;
 import swyp.swyp6_team7.travel.domain.PeriodType;
+import swyp.swyp6_team7.travel.util.TravelSearchConstant;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +40,7 @@ public class TravelSearchCondition {
         this.keyword = keyword;
         //location
         this.genderFilter = getGenderFilter(genderTypes);
-        this.personRangeFilter = personTypes;
+        this.personRangeFilter = getPersonFilter(personTypes);
         this.periodFilter = getPeriodFilter(periodTypes);
         this.tags = tags == null ? new ArrayList<>() : tags;
     }
@@ -48,14 +49,29 @@ public class TravelSearchCondition {
         if (genderTypes == null) {
             return new ArrayList<>();
         }
-        return genderTypes.stream().distinct().map(GenderType::of).toList();
+        return genderTypes.stream()
+                .distinct().limit(TravelSearchConstant.GENDER_TYPE_COUNT)
+                .map(GenderType::of)
+                .toList();
+    }
+
+    private List<String> getPersonFilter(List<String> personTypes) {
+        if (personTypes == null) {
+            return new ArrayList<>();
+        }
+        return personTypes.stream()
+                .distinct().limit(TravelSearchConstant.PERSON_TYPE_COUNT)
+                .toList();
     }
 
     private List<PeriodType> getPeriodFilter(List<String> periodTypes) {
         if (periodTypes == null) {
             return new ArrayList<>();
         }
-        return periodTypes.stream().distinct().map(PeriodType::of).toList();
+        return periodTypes.stream()
+                .distinct().limit(TravelSearchConstant.PERIOD_TYPE_COUNT)
+                .map(PeriodType::of)
+                .toList();
     }
 
 }
