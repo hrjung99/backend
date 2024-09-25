5481ba8e348548e14ae9b1f5798a8df641342988
sujnkim <blackout3210@naver.com>
2024-09-25 13:09:16 +0900

    feat(travel): 여행콘텐츠 추천목록 가나다순 정렬 추가

--- src/main/java/swyp/swyp6_team7/travel/util/TravelRecommendComparator.java
+++ src/main/java/swyp/swyp6_team7/travel/util/TravelRecommendComparator.java
@@ -9,6 +9,9 @@ public class TravelRecommendComparator implements Comparator<TravelRecommendDto>
     @Override
     public int compare(TravelRecommendDto o1, TravelRecommendDto o2) {
         if (o1.getPreferredNumber() == o2.getPreferredNumber()) {
+            if (o1.getRegisterDue().compareTo(o2.getRegisterDue()) == 0) {
+                return o1.getTitle().compareTo(o2.getTitle());
+            }
             return o1.getRegisterDue().compareTo(o2.getRegisterDue());
         }
         return -1 * (o1.getPreferredNumber() - o2.getPreferredNumber());
