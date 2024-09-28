7ff4d68459319c3691a631d45d5e5a8d9da9bc89
sujnkim <blackout3210@naver.com>
2024-09-28 19:19:59 +0900

    feat(notification): 알람 목록 조회 페이징 설정

--- src/main/java/swyp/swyp6_team7/notification/controller/NotificationController.java
+++ src/main/java/swyp/swyp6_team7/notification/controller/NotificationController.java
@@ -1,9 +1,12 @@
 package swyp.swyp6_team7.notification.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import swyp.swyp6_team7.notification.dto.NotificationDto;
 import swyp.swyp6_team7.notification.service.NotificationService;
@@ -18,9 +21,11 @@ public class NotificationController {
 
 
     @GetMapping("/api/notifications")
-    public ResponseEntity<List<NotificationDto>> getNotificationsByUser() {
-        List<NotificationDto> notifications = notificationService.getNotificationsByUser();
-
+    public ResponseEntity<Page<NotificationDto>> getNotificationsByUser(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        Page<NotificationDto> notifications = notificationService.getNotificationsByUser(PageRequest.of(page, size));
         return ResponseEntity.status(HttpStatus.OK)
                 .body(notifications);
     }
--- src/main/java/swyp/swyp6_team7/notification/repository/NotificationRepository.java
+++ src/main/java/swyp/swyp6_team7/notification/repository/NotificationRepository.java
@@ -1,5 +1,7 @@
 package swyp.swyp6_team7.notification.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import swyp.swyp6_team7.notification.entity.Notification;
 
@@ -8,5 +10,7 @@ import java.util.List;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> getNotificationsByReceiverNumberOrderByCreatedAtDesc(int userNumber);
+
+    Page<Notification> getNotificationsByReceiverNumberOrderByCreatedAtDesc(int userNumber, Pageable pageable);
 
 }
