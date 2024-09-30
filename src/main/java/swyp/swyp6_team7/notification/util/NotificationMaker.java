30d6091bac8e44f98ac4788d41f90341db3148da
sujnkim <blackout3210@naver.com>
2024-09-30 18:16:14 +0900

    feat(notification): 여행신청 신청자 알림 추가 및 content 생성 로직 수정

--- src/main/java/swyp/swyp6_team7/notification/util/NotificationMaker.java
+++ src/main/java/swyp/swyp6_team7/notification/util/NotificationMaker.java
@@ -1,6 +1,7 @@
 package swyp.swyp6_team7.notification.util;
 
 import swyp.swyp6_team7.enrollment.domain.Enrollment;
+import swyp.swyp6_team7.member.entity.Users;
 import swyp.swyp6_team7.notification.entity.Notification;
 import swyp.swyp6_team7.notification.entity.NotificationMessageType;
 import swyp.swyp6_team7.notification.entity.TravelNotification;
@@ -8,11 +9,23 @@ import swyp.swyp6_team7.travel.domain.Travel;
 
 public class NotificationMaker {
 
-    public static Notification travelEnrollmentMessage(Travel targetTravel) {
+    public static Notification travelEnrollmentMessageToHost(Travel targetTravel) {
         return TravelNotification.builder()
                 .receiverNumber(targetTravel.getUserNumber())
+                .title(NotificationMessageType.TRAVEL_ENROLL_HOST.getTitle())
+                .content(NotificationMessageType.TRAVEL_ENROLL_HOST.getContent(targetTravel.getTitle()))
+                .travelNumber(targetTravel.getNumber())
+                .travelTitle(targetTravel.getTitle())
+                .travelDueDate(targetTravel.getDueDate())
+                .isRead(false)
+                .build();
+    }
+
+    public static Notification travelEnrollmentMessage(Travel targetTravel, Users users) {
+        return TravelNotification.builder()
+                .receiverNumber(users.getUserNumber())
                 .title(NotificationMessageType.TRAVEL_ENROLL.getTitle())
-                .content(targetTravel.getTitle() + "에 참가 신청자가 있어요. 알림을 눌러 확인해보세요.")
+                .content(NotificationMessageType.TRAVEL_ENROLL.getContent(targetTravel.getTitle()))
                 .travelNumber(targetTravel.getNumber())
                 .travelTitle(targetTravel.getTitle())
                 .travelDueDate(targetTravel.getDueDate())
@@ -24,7 +37,7 @@ public class NotificationMaker {
         return TravelNotification.builder()
                 .receiverNumber(enrollment.getUserNumber())
                 .title(NotificationMessageType.TRAVEL_ACCEPT.getTitle())
-                .content(targetTravel.getTitle() + "에 참가가 확정되었어요. 멤버 댓글을 통해 인사를 나눠보세요.")
+                .content(NotificationMessageType.TRAVEL_ACCEPT.getContent(targetTravel.getTitle()))
                 .travelNumber(targetTravel.getNumber())
                 .travelTitle(targetTravel.getTitle())
                 .travelDueDate(targetTravel.getDueDate())
@@ -36,7 +49,7 @@ public class NotificationMaker {
         return TravelNotification.builder()
                 .receiverNumber(enrollment.getUserNumber())
                 .title(NotificationMessageType.TRAVEL_REJECT.getTitle())
-                .content(targetTravel.getTitle() + "에 참가가 아쉽게도 거절되었어요. 다른 여행을 찾아볼까요?")
+                .content(NotificationMessageType.TRAVEL_REJECT.getContent(targetTravel.getTitle()))
                 .travelNumber(targetTravel.getNumber())
                 .travelTitle(targetTravel.getTitle())
                 .travelDueDate(targetTravel.getDueDate())
