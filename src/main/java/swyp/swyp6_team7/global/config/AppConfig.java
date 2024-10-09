615f0607c97dfd305650a1253061404f40f4ceb6
Yujeong <22mallow3@gmail.com>
2024-10-09 23:44:45 +0900

    feat: RestTemplate 빈 주입

---
+++ src/main/java/swyp/swyp6_team7/global/config/AppConfig.java
@@ -0,0 +1,13 @@
+package swyp.swyp6_team7.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
