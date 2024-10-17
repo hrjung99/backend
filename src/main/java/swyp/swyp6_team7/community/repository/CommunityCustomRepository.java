d27b4ec46215f18eeeeb456aead4d81788af2539
paramy <rimiajung@gmail.com>
2024-10-18 01:05:22 +0900

    fix:커뮤니티 목록 조회 정렬안됨/페이징 안됨/썸네일 안보임 오류 수정

--- src/main/java/swyp/swyp6_team7/community/repository/CommunityCustomRepository.java
+++ src/main/java/swyp/swyp6_team7/community/repository/CommunityCustomRepository.java
@@ -1,13 +1,13 @@
 package swyp.swyp6_team7.community.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import swyp.swyp6_team7.community.dto.response.CommunitySearchCondition;
 import swyp.swyp6_team7.community.dto.response.CommunitySearchDto;
+
+import java.util.List;
 
 public interface CommunityCustomRepository {
 
     void incrementViewCount(int postNumber);
 
-    Page<CommunitySearchDto> search(PageRequest pageRequest, CommunitySearchCondition searchCondition);
+    List<CommunitySearchDto> search(CommunitySearchCondition searchCondition);
 }
