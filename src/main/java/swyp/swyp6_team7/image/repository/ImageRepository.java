7ba012ef9acc67ad4f6db6cdd8335009a0c9006b
sujnkim <blackout3210@naver.com>
2024-10-18 18:48:24 +0900

    feat(image): userNumber로 profile url을 찾는 Repository 메서드 추가

--- src/main/java/swyp/swyp6_team7/image/repository/ImageRepository.java
+++ src/main/java/swyp/swyp6_team7/image/repository/ImageRepository.java
@@ -1,6 +1,8 @@
 package swyp.swyp6_team7.image.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import swyp.swyp6_team7.image.domain.Image;
 
@@ -13,13 +15,19 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
     // 관련 타입과 관련 번호로 단일 이미지를 찾는 메소드
     Optional<Image> findByRelatedTypeAndRelatedNumber(String relatedType, int relatedNumber);
 
+    @Query("select i.url from Image i where i.relatedNumber = :userNumber and i.relatedType = 'profile' and i.order = 0")
+    Optional<String> findUrlByRelatedUserNumber(@Param("userNumber") int userNumber);
+
     // 관련 타입이 community일 때 여러 이미지를 찾는 메소드
     List<Image> findAllByRelatedTypeAndRelatedNumber(String relatedType, int relatedNumber);
 
     //key로 이미지찾기
     Optional<Image> findByRelatedTypeAndRelatedNumberAndKey(String relatedType, int relatedNumber, String key);
+
     Optional<Image> findByRelatedTypeAndRelatedNumberAndOrder(String relatedType, int relatedNumber, int order);
+
     Optional<Image> findByKey(String key);
+
     Optional<Image> findByUrl(String url);
 
 
